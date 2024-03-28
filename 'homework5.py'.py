57ec727f84f9c0727c101be9040e2e57fd443802
matagir <165258966+matagir@users.noreply.github.com>
2024-03-28 15:14:15 +0500

    Add files via upload

---
+++ 'homework5.py'.py
@@ -0,0 +1,13 @@
+my_list = ['Яблоко','Груша','Банан','Апельсин','Киви']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[3:5])
+my_list [3]='Арбуз'
+print(my_list)
+
+my_dict = {'Яблоко':'Appel', 'Банан':'Banana'}
+print(my_dict)
+print(my_dict['Яблоко'])
+my_dict ['Апельсин'] = 'Orange'
+print(my_dict)
